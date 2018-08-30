fedccb603131c02ef1dc3f4b90b35ad3dea2d2d2
AThornton <amanda.thornton@wisc.edu>
2018-08-30 15:02:30 -0500

    allows -a flag for all gdp data files

--- gdp_plots.py
+++ gdp_plots.py
@@ -3,6 +3,7 @@ import pandas
 # because we are no longer in a notebook
 import matplotlib.pyplot as plt
 import sys
+import glob
 
 # load data and transpose so that country names are
 # the columns and their gdp data becomes the rows
@@ -10,7 +11,11 @@ import sys
 # read data into a pandas dataframe and transpose
 #filename = "gapminder_gdp_oceania.csv"
 #filename = sys.argv[1] #first parameter after scriptname
-file_list = sys.argv[1:]
+
+if sys.argv[1] == '-a' :
+	file_list = glob.glob("*gdp*.csv")
+else:
+	file_list = sys.argv[1:]
 
 for filename in file_list:
 	data = pandas.read_csv(filename, index_col = 'country').T
