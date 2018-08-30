90a55cf8b3a89bb78d44f70cae9c6340790a1c29
AThornton <amanda.thornton@wisc.edu>
2018-08-30 14:02:11 -0500

    add filename parameter, command line arg for file

--- gdp_plots.py
+++ gdp_plots.py
@@ -2,15 +2,27 @@ import pandas
 # we need to import part of matplotlib
 # because we are no longer in a notebook
 import matplotlib.pyplot as plt
+import sys
 
 # load data and transpose so that country names are
 # the columns and their gdp data becomes the rows
 
 # read data into a pandas dataframe and transpose
-data = pandas.read_csv('gapminder_gdp_oceania.csv', index_col = 'country').T
+filename = "gapminder_gdp_oceania.csv"
+filename = sys.argv[1] #first parameter after scriptname
+
+data = pandas.read_csv(filename, index_col = 'country').T
 
 # create a plot the transposed data
-ax = data.plot()
+ax = data.plot(title=filename)
+
+# axes labels
+ax.set_xlabel('Year')
+ax.set_ylabel('GDP Per Capita')
+
+# set axes ticks
+ax.set_xticks(range(len(data.index)))
+ax.set_xticklabels(data.index,rotation=45)
 
 # display the plot
 plt.show()
